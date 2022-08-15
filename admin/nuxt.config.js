02b93a22a7809dff660dd045b03189716618460e
alaminrony <alaminrony100@gmail.com>
2022-08-16 02:06:01 +0600

    dashboard setup + login system done

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -1,44 +1,94 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'spa',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
   head: {
-    title: 'admin',
-    htmlAttrs: {
-      lang: 'en'
+    title: 'online shop dashboard',
+    bodyAttrs: {
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'online shop dashboard' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'stylesheet', type: 'text/css', href: '/css/font-face.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/vendor/font-awesome-4.7/css/font-awesome.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/vendor/font-awesome-5/css/fontawesome-all.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/vendor/mdi-font/css/material-design-iconic-font.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/vendor/bootstrap-4.1/bootstrap.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/vendor/css-hamburgers/hamburgers.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: '/css/theme.css' }
+    ],
+    script: [
+      {
+        src: '/vendor/jquery-3.2.1.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: '/vendor/bootstrap-4.1/popper.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src: '/vendor/bootstrap-4.1/bootstrap.min.js',
+        type: 'text/javascript'
+      }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  /*
+  ** Global CSS
+  */
   css: [
   ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
   plugins: [
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  /*
+  ** Nuxt.js dev-modules
+  */
   buildModules: [
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // Doc: https://bootstrap-vue.js.org
+    // 'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
   ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    baseURL: 'http://localhost:8000'
+  },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
   build: {
+
   },
   srcDir: __dirname,
   buildDir: '.nuxt/admin'
